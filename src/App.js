fcfeb6247221f051402f5a62782f683d663c44ef
Haur Kang <haur.kang@gmail.com>
2019-01-21 22:46:31 +0000

    creates routers

--- src/App.js
+++ src/App.js
@@ -7,15 +7,21 @@ import Default from './components/Default';
 import Details from './components/Details';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Switch>
-          
-        </Switch>
+        <Navbar>
+          <Switch>
+              <Route exact path="/" component={ProductList}/>
+              <Route path="/details" component={Details}/>
+              <Route path="/cart" component={Cart}/>
+              <Route component={Default}/>
+          </Switch>
+        </Navbar>
       </React.Fragment>
     );
   }
