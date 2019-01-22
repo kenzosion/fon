818efb07d4598294e215484249da8591139b84e4
Haur Kang <haur.kang@gmail.com>
2019-01-22 22:30:59 +0000

    adds Link from react router dom to navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import Switch from "react-switch";
+// import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Cart from './components/Cart';
@@ -7,21 +8,20 @@ import Default from './components/Default';
 import Details from './components/Details';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar>
+        <Navbar />
           <Switch>
               <Route exact path="/" component={ProductList}/>
               <Route path="/details" component={Details}/>
               <Route path="/cart" component={Cart}/>
               <Route component={Default}/>
           </Switch>
-        </Navbar>
       </React.Fragment>
     );
   }
