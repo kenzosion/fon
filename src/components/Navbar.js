5d006911ffd602c7a55b1c54a2b722f1620489b1
Haur Kang <haur.kang@gmail.com>
2019-01-25 16:05:04 +0000

    imports styled component to navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
+import styled from 'styled-components';
 
 
 export default class Navbar extends Component {
