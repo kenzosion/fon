e0a8f35fd6a6376f9fe2fe8067b22750b0020aab
Haur Kang <haur.kang@gmail.com>
2019-01-29 22:54:28 +0000

    adds title component to productlist component

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ export default function Title({ name, title}) {
     <div className="row">
       <div className="col-10 mx-auto my-2 text-center">
       <h1 className="font-weight-bold">
-        
+        {name} <strong> {title} </strong>
       </h1>
       </div>
     </div>
